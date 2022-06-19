3a7d08cd96d385e11a0ae463ebac4baf4483cac0
Jack Christopher <jhuaihuah@unsa.edu.pe>
2022-06-18 20:42:32 -0500

    Added: Image from matplotlib rendered

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os 
+import preprocessing as pre
 from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
@@ -6,7 +7,11 @@ app.secret_key = 'very-secret-key'
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    p = pre.Preprocessing()
+    plt = p.get_missing_values_plot()
+    plt.savefig('static/img/missing_values.png')
+
+    return render_template("index.html", name="missing_values.png")
 
 @app.route('/favicon.ico') 
 def favicon(): 
